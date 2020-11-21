46325ae8d5086f1768672e14b088fbcb01591a72
savan20 <bsp4145@gmail.com>
2020-11-21 10:08:27 +0530

    I did it

---
+++ src/com/first/savan/Main.java
@@ -0,0 +1,9 @@
+package com.first.savan;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Yoooooo!!!! i Did It :D");
+    }
+}
